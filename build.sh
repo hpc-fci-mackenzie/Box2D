cc91e71ed68fc50e1378688dccb05f2063062312
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2018-06-11 21:49:58 -0300

    Add a script to build on MacOS

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+mkdir -p bin
+cd bin
+curl -L "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha12/premake-5.0.0-alpha12-macosx.tar.gz" -o premake-5.0.0-alpha12-macosx.tar.gz
+tar xzf premake-5.0.0-alpha12-macosx.tar.gz
+cd ../Box2D
+../bin/premake5 xcode4
+cd Build
+for i in $( grep -R "C++11" * | cut -d : -f1 ); do
+	echo $i
+	sed -i '' -e "s/C\+\+11/c\+\+11/g" $i
+done
+xcodebuild -project Box2D.xcodeproj -configuration Release -verbose
