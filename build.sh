50e649509786061fcf9e2f50d70c92da6f7d223e
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2018-06-11 21:59:00 -0300

    Remove debug information & add Travis-CI language+compiler

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 mkdir -p bin
 cd bin
 curl -L "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha12/premake-5.0.0-alpha12-macosx.tar.gz" -o premake-5.0.0-alpha12-macosx.tar.gz
@@ -7,7 +7,6 @@ cd ../Box2D
 ../bin/premake5 xcode4
 cd Build
 for i in $( grep -R "C++11" * | cut -d : -f1 ); do
-	echo $i
 	sed -i '' -e "s/C\+\+11/c\+\+11/g" $i
 done
 xcodebuild -project Box2D.xcodeproj -configuration Release -verbose
